cbf50a394fa31cdc825464436985fee903dfc13c
birkofcz <stepan.benes@gmail.com>
2023-11-06 12:00:17 +0100

    refactoring v2 -working

--- version2 - working/srcs/requirements/wordpress/conf/create_wordpress.sh
+++ version2 - working/srcs/requirements/wordpress/conf/create_wordpress.sh
@@ -5,8 +5,6 @@ then
 	echo "Wordpress already downloaded"
 else
 
-######## MANDATORY ########
-
 	wget http://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	mv wordpress/* .
@@ -19,15 +17,6 @@ else
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
 
-# ######## BONUS ########
-# ## Redis Çerez Ayarları ##
-# 	wp config set WP_REDIS_HOST redis --allow-root
-#   	wp config set WP_REDIS_PORT 6379 --raw --allow-root
-#  	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-#  	wp config set WP_REDIS_CLIENT phpredis --allow-root
-# 	wp plugin install redis-cache --activate --allow-root
-#         wp plugin update --all --allow-root
-# 	wp redis enable --allow-root
 fi
 
 exec "$@"
